3c0cba2c9b208a9aea0cd3818c54d1079abdccd3
Shreyash Sagar <shreyashsagar2005@gmail.com>
2026-06-27 15:39:44 +0530

    chore: configure github pages deployment base path and workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Legendary_Burgers/',
   plugins: [
     react(),
     tailwindcss(),
